92ed9e89ab5059e251ea5877177553263adf1c94
Kimjaegwun <rlawornjs4@naver.com>
2023-02-27 22:18:28 +0900

    contents add

--- components/Daadok.tsx
+++ components/Daadok.tsx
@@ -33,20 +33,40 @@ const Daadok = () => {
         </div>
       </div>
       <div className="mt-5">
-        <div className="mb-2 text-orange-200">다독, 정기 구독 서비스 런칭</div>
         <Image
           src={"/daadok/daadok-detail.webp"}
           width={1252}
           height={778}
           placeholder="blur"
           blurDataURL={BLUR_URL}
-          className="rounded-xl"
+          className="rounded-xl mb-2"
           alt="daadok"
         />
-        <div className="my-2">
-          앱-웹 서비스 기획, 클라이언트-어드민 페이지 및 API 구현
+        <div className="text-orange-200">다독, 정기 구독 서비스 런칭</div>
+        <div className="text-gray-400 mt-2">[Tech Stack]</div>
+        <div className="min-w-[60px] text-teal-200">✓ React:</div>
+        <div className="ml-2">
+          웹/앱 모두를 아우를 수 있고 앱에서는 더욱이 크로스 플랫폼 지원을 고려
+          <br />
+          react 와 react-native와는 문법에서는 차이가 있지만 비즈니스 로직은
+          공유 가능한 점
         </div>
-        <div className="grid grid-cols-2">
+        <div className="min-w-[85px] text-teal-200">✓ Graph-QL:</div>
+        <div className="ml-2">
+          graphql은 하나의 엔드 포인트로 인한 api 호출의 편리성 및 schema에
+          정의되어 있는 걸 토대로 작동하기 때문에 백엔드와의 소통을 최소화할 수
+          있다는 점<br /> apollo 라이브러리를 통해 캐싱, playground,
+          subscription 등을 편리하게 이용가능
+        </div>
+        <div className="text-gray-400 mt-2">[구현 방안]</div>
+        <div>
+          - 핵심적인 기능 파악 및 확장성 있는 컴포넌트를 구현
+          <br /> - 적은 인원으로 빠르게 개발하기 위해 추가 리소스가 들어가는
+          방향을 지양 <br />- 문서 작성 시에는 세부 기능별로 정리하기보다는
+          프로세스 단위(로그인, 구독, 결제 등)로 정리하여 각 단계별로 어떤
+          기능을 넣어야할지 정의하며 작성
+        </div>
+        <div className="grid grid-cols-2 mt-2">
           <Link
             href={"/daadok/daadok-detail2.webp"}
             target="_blank"
@@ -80,13 +100,12 @@ const Daadok = () => {
         </div>
         <div className="text-gray-400 mt-2">[Work]</div>
         <div>
-          - Next.js/React Native를 이용하여 로그인, 메인, 상품 페이지 등
-          전반적인 화면 구성 및 API 연동
+          - 로그인, 메인, 상품 페이지 등 전반적인 화면 구성 및 API 연동
+          <br /> - postgreSQL을 통한 디비 및 서버 환경 구축 / git 연동 <br /> -
+          codepush 적용을 통한 실시간 배포 환경 구축 <br /> - figma을 이용한
+          디자이너와의 협업 및 퍼블리싱
+          <br />- node-cron을 통한 정기배포 서비스 구축
         </div>
-        <div>- NodeJS, GraphQL 서버 환경 구축 및 Git 연동</div>
-        <div>- Jenkins, Docker 를 통한 배포 시스템 자동화</div>
-        <div>- CodePush 적용을 통한 실시간 트러블슈팅 작업 환경 구축</div>
-        <div>- Figma을 이용한 디자이너와의 협업 및 Frontend 퍼블리싱</div>
         <Link
           href={"/daadok-detail4.webp"}
           target="_blank"
@@ -102,7 +121,6 @@ const Daadok = () => {
             alt="daadok"
           />
         </Link>
-        <div>- node-cron을 통한 정기배포 서비스 구축</div>
         <div className="text-gray-400 my-2">[Result]</div>
         <div>클라이언트 및 어드민 웹 배포, 앱 프로덕션 출시</div>
         <div>회원가입자 3300명, 협력업체 50여 곳 달성</div>
