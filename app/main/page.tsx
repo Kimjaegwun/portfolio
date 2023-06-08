dc947239bc97f907ff4f06db3df16509a3d644c7
kimjaegwun <rlawornjs4@naver.com>
2023-06-08 17:56:57 +0900

    rev

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -7,9 +7,6 @@ const MainServerPage = () => {
   return (
     <>
       <SEO title="메인 페이지" description="jaegwun's portfolio!" />
-      <Link href={"/detail/53"}>
-        <div>123123</div>
-      </Link>
       <Main />
     </>
   );
