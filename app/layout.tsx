782455b5f67ceabdfff154d2640e15b5da4f2f3e
kimjaegwun <rlawornjs4@naver.com>
2023-06-14 14:13:47 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-"use client";
-
 import "./globals.css";
 
-import { Analytics } from "@vercel/analytics/react";
-import { AnimatePresence } from "framer-motion";
-import Header from "@/components/Header";
+import { Metadata } from "next";
+import RootClientLayout from "./layout.client";
 import localFont from "@next/font/local";
-import { usePathname } from "next/navigation";
 
 const roboto = localFont({
   src: [
@@ -29,23 +25,18 @@ const roboto = localFont({
   variable: "--font-yanolja",
 });
 
+export const metadata: Metadata = {
+  title: "JaeGwun.Kim",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const path = usePathname();
-
   return (
     <html lang="en" className={`${roboto.variable}`}>
-      <title>JaeGwun.Kim</title>
-      <body className="bg-[#202023]">
-        <main className="font-yanolja text-[16px]">
-          {path !== "/" && <Header />}
-          <AnimatePresence initial={true}>{children}</AnimatePresence>
-          <Analytics />
-        </main>
-      </body>
+      <RootClientLayout>{children}</RootClientLayout>
     </html>
   );
 }
