f7accef46592a0fc62eefdd9b612426dd0139df2
kimjaegwun <rlawornjs4@naver.com>
2023-06-14 14:07:27 +0900

    seo test

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import SEO from "../seo";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <SEO title="메인 - 자기소개" description="jaegwun's profile" />
-      <div className="mx-auto max-w-[700px] px-[10px] h-screen p-3 text-white">
-        {children}
-      </div>
-    </>
+    <div className="mx-auto max-w-[700px] px-[10px] h-screen p-3 text-white">
+      {children}
+    </div>
   );
 };
 
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,9 +1,11 @@
 import Main from "@/components/Main";
 import React from "react";
+import SEO from "../seo";
 
 const MainServerPage = () => {
   return (
     <>
+      <SEO title="메인 - 자기소개" description="jaegwun's profile" />
       <Main />
     </>
   );
