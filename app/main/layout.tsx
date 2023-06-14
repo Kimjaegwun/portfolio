4efd0090c886fefd55f0f7df222d61ef1531c7cd
kimjaegwun <rlawornjs4@naver.com>
2023-06-14 13:38:03 +0900

    metadata location rev

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,11 +1,18 @@
+import { Metadata } from "next";
 import React from "react";
-import SEO from "@/app/seo";
+
+export const metadata: Metadata = {
+  title: "메인 - 자기소개",
+  description: "jaegwun's profile",
+};
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto max-w-[700px] px-[10px] h-screen p-3 text-white">
-      {children}
-    </div>
+    <>
+      <div className="mx-auto max-w-[700px] px-[10px] h-screen p-3 text-white">
+        {children}
+      </div>
+    </>
   );
 };
 
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,5 @@
 import Main from "@/components/Main";
-import { Metadata } from "next";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "메인 - 자기소개",
-  description: "jaegwun's profile",
-};
 
 const MainServerPage = () => {
   return <Main />;
